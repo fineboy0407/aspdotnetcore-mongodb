2e11a32a622a10974be1923dc2cb85e138552e55
fineboy0407 <levilee.fine@gmail.com>
2022-12-08 23:20:57 +0500

    Add Swagger Config

--- CoreMongo/Program.cs
+++ CoreMongo/Program.cs
@@ -17,6 +17,15 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "CoreMongo",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
